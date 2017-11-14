e57779695ad13e4ace000d86204926818f2b7498
ekim62 <32079727+ekim62@users.noreply.github.com>
2017-11-13 23:23:21 -0500

    Update update.php

--- update.php
+++ update.php
@@ -8,18 +8,16 @@
 	$connection = new mysqli('66.147.242.186', 'urcscon3_sydney', 'coffee1N', 'urcscon3_sydney');
 
 	$counter = $_POST['counter'];
-	$fname = Trim(stripslashes($_POST['first']));
-	$lname = Trim(stripslashes($_POST['last']));
+	$fname = Trim(stripslashes($_POST['fname']));
 	$email = Trim(stripslashes($_POST['email']));
 	$pnumber = Trim(stripslashes($POST['pnumber']));
 	
 	$fname = mysqli_real_excape_string($connection, $fname);
-	$lname = mysqli_real_excape_string($connection, $lname);
 	$email = mysqli_real_excape_string($connection, $email);
 	$pnumber = mysqli_real_excape_string($connection, $pnumber);
 
 	$query = "UPDATE survey SET";
-	$query .= "fname = '$fname', lname = '$lname', email ='$email', pnumber = $'pnumber' ";
+	$query .= "fname = '$fname', email ='$email', pnumber = $'pnumber' ";
 	$query .= "WHERE counter = {$ounter}";
 	$result = mysqli_query($connection, $query);
 ?>
